11b6b02e80e2e30ef2fc77474699c0c241f0fa78
Christos Bisias <christosbis@gmail.com>
2025-08-24 12:54:00 +0300

    handle_env.sh + observability config

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # airflow-summit-2025-demo
 
+## Airflow docker setup
 
 https://airflow.apache.org/docs/apache-airflow/stable/howto/docker-compose/index.html
+
+## Airflow webserver
+
+| Type | Value |
+| ----------- | ----------- |
+| Url | http://0.0.0.0:8080 |
+| Username | `airflow` |
+| Password | `airflow` |
+
+## Jaeger
+
+| Type | Value |
+| ----------- | ----------- |
+| Url | http://localhost:16686/search |
+
+## Execute the demo
+
+```bash
+
+```
+
+
+
---
+++ docker-env/handle_env.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+abs_path=$1
+action=${2:-"start"}
+
+CURRENT_PROJECT="airflow-summit-2025-demo"
+
+cd "$abs_path"/"$CURRENT_PROJECT"/docker-env/compose
+
+# The env variable is enough for docker compose to use the files.
+export COMPOSE_FILE="docker-compose.yaml:observability.yaml"
+
+if [[ "$action" == "start" ]]; then
+  docker compose up -d
+elif [[ "$action" == "stop" ]]; then
+  docker compose down
+elif [[ "$action" == "restart" ]]; then
+  docker compose down
+  docker compose up -d
+else
+  echo "Unknown action -- '$action'. Try one of the following: 'start', 'stop', 'restart'."
+fi
+
